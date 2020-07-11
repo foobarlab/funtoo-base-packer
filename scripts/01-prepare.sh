5b363d2d1db9eac71fc7bf5defdf8b2fd06b53e4
Martin Eisenbarth <eyesee@foobarlab.net>
2020-07-11 10:17:25 +0200

    update motd

--- scripts/01-prepare.sh
+++ scripts/01-prepare.sh
@@ -60,7 +60,7 @@ sudo eselect python list
 sudo rm -f /etc/motd
 cat <<'DATA' | sudo tee -a /etc/motd
 
-Funtoo GNU/Linux (BUILD_BOX_NAME) - release BUILD_BOX_VERSION build BUILD_TIMESTAMP
+Funtoo GNU/Linux Vagrant Box (BUILD_BOX_NAME) - release BUILD_BOX_VERSION build BUILD_TIMESTAMP
 
 DATA
 sudo sed -i 's/BUILD_BOX_NAME/'"$BUILD_BOX_NAME"'/g' /etc/motd
