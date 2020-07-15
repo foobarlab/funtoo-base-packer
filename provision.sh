f56ed601372bd5c7e1e29d96610023be24267e8b
Martin Eisenbarth <eyesee@foobarlab.net>
2020-07-15 23:27:56 +0200

    renamed scipts, add tools

--- provision.sh
+++ provision.sh
@@ -12,16 +12,16 @@ fi
 chmod +x $SCRIPTS/scripts/*.sh
 
 for script in \
-  01-prepare \
-  02-kernel \
-  03-system-update \
-  04-gcc-upgrade \
-  05-rebuild-system \
-  06-software \
-  07-spectre \
-  08-ansible \
-  09-postprocess \
-  10-cleanup
+  10-prepare \
+  20-kernel \
+  30-system-update \
+  40-gcc-upgrade \
+  50-rebuild-system \
+  60-software \
+  70-spectre \
+  80-ansible \
+  90-postprocess \
+  99-cleanup
 do
   echo "==============================================================================="
   echo " >>> Running $script.sh"
