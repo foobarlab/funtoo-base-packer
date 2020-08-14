462f175591a43327503a7bd0c59c59fdc6416172
Martin Eisenbarth <eyesee@foobarlab.net>
2020-08-14 12:31:19 +0200

    config for upgraded Ansible 2.9

--- scripts/80-ansible.sh
+++ scripts/80-ansible.sh
@@ -19,9 +19,9 @@ fi
 sudo emerge -vt app-admin/ansible
 sudo mkdir -p /etc/ansible
 cat <<'DATA' | sudo tee -a /etc/ansible/ansible.cfg
-# disable SSH host key checking, see: https://www.vagrantup.com/docs/provisioning/ansible_local.html
 [defaults]
-host_key_checking = no
+host_key_checking = no				# disable SSH host key checking, see: https://www.vagrantup.com/docs/provisioning/ansible_local.html
+interpreter_python = auto_silent	# Python discovery, see: https://docs.ansible.com/ansible/latest/reference_appendices/interpreter_discovery.html
 [ssh_connection]
 ssh_args = -o ControlMaster=auto -o ControlPersist=60s -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes
 DATA
