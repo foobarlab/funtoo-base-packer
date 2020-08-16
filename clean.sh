e74a7fe6d40f2358ee460a9125c11d0e2a64504d
Martin Eisenbarth <eyesee@foobarlab.net>
2020-08-16 23:54:09 +0200

    be more verbose during cleanup, drop build_number

--- clean.sh
+++ clean.sh
@@ -11,19 +11,21 @@ vagrant destroy -f || true
 echo "Removing box '$BUILD_BOX_NAME' ..."
 vagrant box remove -f "$BUILD_BOX_NAME" 2>/dev/null || true
 echo "Cleaning .vagrant dir ..."
-rm -rf .vagrant/ || true
+rm -vrf .vagrant/ || true
 echo "Cleaning packer_cache ..."
-rm -rf packer_cache/ || true
+rm -vrf packer_cache/ || true
 echo "Cleaning packer output-virtualbox-ovf dir ..."
-rm -rf output-virtualbox-ovf || true
+rm -vrf output-virtualbox-ovf || true
+echo "Drop build number ..."
+rm -vf build_number || true
 echo "Drop build version ..."
-rm -f build_version || true
+rm -vf build_version || true
 echo "Drop major version ..."
-rm -f version || true
+rm -vf version || true
 echo "Deleting any box file ..."
-rm -f *.box || true
+rm -vf *.box || true
 echo "Cleanup old logs ..."
-rm -f *.log || true
+rm -vf *.log || true
 echo "Cleanup broken wget downloads ..."
-rm -f download || true
+rm -vf download || true
 echo "All done. You may now run './build.sh' to build a fresh box."
