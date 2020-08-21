99a90b541e96e14b91cd41a4086e88568a4c4c51
Martin Eisenbarth <eyesee@foobarlab.net>
2020-08-21 11:17:36 +0200

    no need to suspend

--- startup.sh
+++ startup.sh
@@ -6,8 +6,6 @@ command -v vagrant >/dev/null 2>&1 || { echo "Command 'vagrant' required but it'
 
 echo "==> Starting '$BUILD_BOX_NAME' box ..."
 
-echo "Suspending any running instances ..."
-vagrant suspend
 echo "Powerup '$BUILD_BOX_NAME' ..."
 vagrant up --no-provision || { echo "Unable to startup '$BUILD_BOX_NAME'."; exit 1; }
 echo "Establishing SSH connection to '$BUILD_BOX_NAME' ..."
