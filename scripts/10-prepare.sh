479cad40e183d45ca44c61703476b044a04d2557
Martin Eisenbarth <eyesee@foobarlab.net>
2020-09-03 23:29:33 +0200

    add INSTALL_MASK to prevent some locales being installed

--- scripts/10-prepare.sh
+++ scripts/10-prepare.sh
@@ -19,8 +19,12 @@ sed -i 's/<br>/\n/g' ~vagrant/.release_$BUILD_BOX_NAME
 sudo sed -i 's/USE=\"/USE="idn lzma tools udev syslog cacert threads pic gold ncurses /g' /etc/portage/make.conf
 
 cat <<'DATA' | sudo tee -a /etc/portage/make.conf
-# adding some flags for CPUs newer than 2011 (intel-nehalem/amd-bulldozer)
+
+# experimental: add some flags for CPUs after 2011 (intel-nehalem/amd-bulldozer)
 CPU_FLAGS_X86="${CPU_FLAGS_X86} popcnt sse3 sse4_1 sse4_2 ssse3"
+
+# save some space: just install locales "en", "en_US", "de", "fr"
+INSTALL_MASK="/usr/share/locale -/usr/share/locale/en -/usr/share/locale/en_US -/usr/share/locale/de -/usr/share/locale/fr"
 
 # added here, not in profiles:
 VIDEO_CARDS="virtualbox"
