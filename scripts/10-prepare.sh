e867735e3cbcd8a1737838a0d8b6174942e23892
Martin Eisenbarth <eyesee@foobarlab.net>
2020-09-04 06:30:41 +0200

    experimental

--- scripts/10-prepare.sh
+++ scripts/10-prepare.sh
@@ -21,10 +21,10 @@ sudo sed -i 's/USE=\"/USE="idn lzma tools udev syslog cacert threads pic gold nc
 cat <<'DATA' | sudo tee -a /etc/portage/make.conf
 
 # experimental: add some flags for CPUs after 2011 (intel-nehalem/amd-bulldozer)
-CPU_FLAGS_X86="${CPU_FLAGS_X86} popcnt sse3 sse4_1 sse4_2 ssse3"
+#CPU_FLAGS_X86="${CPU_FLAGS_X86} popcnt sse3 sse4_1 sse4_2 ssse3"
 
-# save some space: just install locales "en", "en_US", "de", "fr"
-INSTALL_MASK="/usr/share/locale -/usr/share/locale/en -/usr/share/locale/en_US -/usr/share/locale/de -/usr/share/locale/fr"
+# testing: save some space: just install locales "en", "en_US", "de", "fr"
+#INSTALL_MASK="/usr/share/locale -/usr/share/locale/en -/usr/share/locale/en_US -/usr/share/locale/de -/usr/share/locale/fr"
 
 # added here, not in profiles:
 VIDEO_CARDS="virtualbox"
