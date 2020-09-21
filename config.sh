08e3d8d82b3826197f1079ef8287bc980df4f137
Martin Eisenbarth <eyesee@foobarlab.net>
2020-09-21 11:27:54 +0200

    keep max 3 boxes

--- config.sh
+++ config.sh
@@ -30,7 +30,7 @@ export BUILD_INCLUDE_ANSIBLE=true         # if true, include Ansible for automat
 export BUILD_WINDOW_SYSTEM=true           # build X window system (X.Org)
 export BUILD_HEADLESS=false               # if false, gui will be shown
 
-export BUILD_KEEP_MAX_CLOUD_BOXES=1       # set the maximum number of boxes to keep in Vagrant Cloud
+export BUILD_KEEP_MAX_CLOUD_BOXES=3       # set the maximum number of boxes to keep in Vagrant Cloud
 
 # ----------------------------! do not edit below this line !----------------------------
 
