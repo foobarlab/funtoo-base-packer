5e97dc78b93a020d857960bf1b7c4bd507c00aaa
Martin Eisenbarth <eyesee@foobarlab.net>
2020-09-21 11:12:15 +0200

    xorg setup

--- scripts/65-xorg.sh
+++ scripts/65-xorg.sh
@@ -91,7 +91,7 @@ DATA
 cat <<'DATA' | sudo tee -a /etc/X11/xorg.conf.d/30keyboard.conf
 # set us-international keyboard
 # see: https://blechtog.wordpress.com/2012/05/25/gentoo-config-for-us-international-keyboard-layout/
-# see https://zuttobenkyou.wordpress.com/2011/08/24/xorg-using-the-us-international-altgr-intl-variant-keyboard-layout/
+# see: https://zuttobenkyou.wordpress.com/2011/08/24/xorg-using-the-us-international-altgr-intl-variant-keyboard-layout/
 Section "InputClass"
         Identifier "keyboard-all"
         Option "XkbLayout" "us"
@@ -107,7 +107,7 @@ Section "InputClass"
 EndSection
 DATA
 
-sudo gpasswd -a vagrant video  # FIXME is this needed?
+sudo gpasswd -a vagrant video
 
 # ---- install display / window manager
 
@@ -116,14 +116,165 @@ cat <<'DATA' | sudo tee -a /etc/portage/package.use/base-xorg
 DATA
 
 sudo emerge -vt \
-	x11-wm/fluxbox \
 	x11-misc/lightdm \
-	sys-auth/elogind
+	sys-auth/elogind \
+	x11-wm/fluxbox \
+	x11-themes/fluxbox-styles-fluxmod
 
 sudo sed -i 's/DISPLAYMANAGER=\"xdm\"/DISPLAYMANAGER=\"lightdm\"/g' /etc/conf.d/xdm
 
-# TODO configure lightdm: /etc/lightdm/lightdm.conf
-# TODO configure lightdm: /etc/lightdm/lightdm-gtk-greeter.conf
+# configure lightdm: autologin user 'vagrant'
+sudo sed -i 's/#autologin-user=/autologin-user=vagrant/g' /etc/lightdm/lightdm.conf
+
+# ---- fluxbox config
+
+# TODO put into Ansible role?
+
+mkdir ~vagrant/.fluxbox || true
+
+cp ~vagrant/.fluxbox/startup ~vagrant/.fluxbox/startup.dist || true
+cat <<'DATA' | sudo tee -a ~vagrant/.fluxbox/startup
+#!/bin/sh
+#
+# fluxbox startup-script:
+#
+# Lines starting with a '#' are ignored.
+
+# Change your keymap:
+xmodmap "/home/vagrant/.Xmodmap"
+
+# Applications you want to run with fluxbox.
+# MAKE SURE THAT APPS THAT KEEP RUNNING HAVE AN ''&'' AT THE END.
+#
+# unclutter -idle 2 &
+# wmnd &
+# wmsmixer -w &
+# idesk &
+
+# Enable autoscaling client display:
+sudo /usr/bin/VBoxClient --vmsvga &
+
+# And last but not least we start fluxbox.
+# Because it is the last app you have to run it with ''exec'' before it.
+
+exec fluxbox
+# or if you want to keep a log:
+# exec fluxbox -log "/home/vagrant/.fluxbox/log"
+
+DATA
+#sudo chown vagrant:vagrant ~vagrant/.fluxbox/startup  # FIXME check if needed
+
+cp ~vagrant/.fluxbox/init ~vagrant/.fluxbox/init.dist || true
+cat <<'DATA' | sudo tee -a ~vagrant/.fluxbox/init
+session.screen0.tab.placement:  TopLeft
+session.screen0.tab.width:  64
+session.screen0.clientMenu.usePixmap:   true
+session.screen0.toolbar.height: 0
+session.screen0.toolbar.alpha:  255
+session.screen0.toolbar.onhead: 1
+session.screen0.toolbar.placement:  BottomCenter
+session.screen0.toolbar.visible:    true
+session.screen0.toolbar.widthPercent:   100
+session.screen0.toolbar.maxOver:    false
+session.screen0.toolbar.layer:  Dock
+session.screen0.toolbar.autoHide:   false
+session.screen0.toolbar.tools:  prevworkspace, workspacename, nextworkspace, iconbar, systemtray, clock
+session.screen0.titlebar.left:  Stick
+session.screen0.titlebar.right: Minimize Maximize Close
+session.screen0.iconbar.usePixmap:  true
+session.screen0.iconbar.iconTextPadding:    10
+session.screen0.iconbar.iconWidth:  128
+session.screen0.iconbar.mode:   {static groups} (workspace)
+session.screen0.iconbar.alignment:  Relative
+session.screen0.tabs.usePixmap: true
+session.screen0.tabs.maxOver:   false
+session.screen0.tabs.intitlebar:    true
+session.screen0.slit.layer: Dock
+session.screen0.slit.alpha: 255
+session.screen0.slit.maxOver:   false
+session.screen0.slit.acceptKdeDockapps: true
+session.screen0.slit.autoHide:  false
+session.screen0.slit.placement: RightBottom
+session.screen0.slit.onhead:    0
+session.screen0.window.focus.alpha: 255
+session.screen0.window.unfocus.alpha:   255
+session.screen0.menu.alpha: 255
+session.screen0.workspaces: 4
+session.screen0.maxDisableMove: false
+session.screen0.noFocusWhileTypingDelay:    0
+session.screen0.showwindowposition: false
+session.screen0.fullMaximization:   false
+session.screen0.colPlacementDirection:  TopToBottom
+session.screen0.autoRaise:  true
+session.screen0.strftimeFormat: %k:%M
+session.screen0.tooltipDelay:   500
+session.screen0.workspacewarping:   true
+session.screen0.focusModel: ClickFocus
+session.screen0.rowPlacementDirection:  LeftToRight
+session.screen0.clickRaises:    true
+session.screen0.focusNewWindows:    true
+session.screen0.windowMenu: /home/vagrant/.fluxbox/windowmenu
+session.screen0.opaqueMove: true
+session.screen0.tabFocusModel:  ClickToTabFocus
+session.screen0.allowRemoteActions: false
+session.screen0.workspaceNames: Workspace 1,Workspace 2,Workspace 3,Workspace 4,
+session.screen0.menuDelay:  200
+session.screen0.windowPlacement:    RowMinOverlapPlacement
+session.screen0.edgeSnapThreshold:  10
+session.screen0.maxIgnoreIncrement: true
+session.screen0.defaultDeco:    NORMAL
+session.screen0.maxDisableResize:   false
+session.appsFile:   /home/vagrant/.fluxbox/apps
+session.tabsAttachArea: Window
+session.tabPadding: 0
+session.keyFile:    ~/.fluxbox/keys
+session.colorsPerChannel:   4
+session.ignoreBorder:   false
+session.cacheLife:  5
+session.styleOverlay:   /home/vagrant/.fluxbox/overlay
+session.cacheMax:   200
+session.menuFile:   ~/.fluxbox/menu
+session.doubleClickInterval:    250
+session.menuSearch: itemstart
+session.configVersion:  13
+session.autoRaiseDelay: 250
+session.slitlistFile:   /home/vagrant/.fluxbox/slitlist
+session.styleFile:  /usr/share/fluxbox/fluxmod/styles/Pillow
+session.forcePseudoTransparency:    false
+
+DATA
+#sudo chown vagrant:vagrant ~vagrant/.fluxbox/init  # FIXME check if needed
+
+cp ~vagrant/.fluxbox/overlay ~vagrant/.fluxbox/overlay.dist || true
+cat <<'DATA' | sudo tee -a ~vagrant/.fluxbox/overlay
+! Prevent styles from setting the background:
+background: none
+
+! Override Pillow theme:
+toolbar.clock.color:            #444444
+!toolbar.alpha:                  255
+toolbar.color:                  #444444
+toolbar.button.pressed.color:   #444444
+window.title.focus.color:       #666666
+window.title.unfocus.color:     #666666
+window.handle.focus.color:      #444444
+window.handle.unfocus.color:    #444444
+window.grip.focus.color:        #444444
+window.grip.unfocus.color:      #444444
+window.label.focus.textColor:   #fefefe
+window.label.unfocus.textColor: #fefefe
+!window.alpha:                   255
+menu.title.color:               #666666
+menu.title.textColor:           #fefefe
+menu.hilite.color:              #444444
+menu.hilite.textColor:          #eeeeee
+menu.frame.textColor:           #222222
+menu.itemHeight:                14
+!menu.alpha:                     255
+borderColor:                    #666666
+
+DATA
+#sudo chown vagrant:vagrant ~vagrant/.fluxbox/overlay  # FIXME check if needed
 
 sudo rc-update add xdm default
 
@@ -131,4 +282,16 @@ sudo rc-update add xdm default
 
 sudo emerge -vt \
 	x11-terms/xterm \
-	gnome-extra/nm-applet
+	x11-apps/mesa-progs
+
+# TODO gnome-extra/nm-applet?
+# TODO check gnome compatibility, add NetworkManager?
+
+cat <<'DATA' | sudo tee -a ~vagrant/.Xresources
+xterm*background: black
+xterm*foreground: lightgray
+xterm*font: *-fixed-*-*-*-13-*
+DATA
+#sudo chown vagrant:vagrant ~vagrant/.Xresources  # FIXME check if needed
+
+fluxbox-generate_menu -is -ds
