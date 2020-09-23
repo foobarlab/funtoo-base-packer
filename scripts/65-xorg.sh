0bbc0f2367f0fd2dfaa09d5f514af3c0b01d5e37
Martin Eisenbarth <eyesee@foobarlab.net>
2020-09-23 10:49:54 +0200

    uppercase

--- scripts/65-xorg.sh
+++ scripts/65-xorg.sh
@@ -93,7 +93,7 @@ cat <<'DATA' | sudo tee -a /etc/X11/xorg.conf.d/30keyboard.conf
 # see: https://blechtog.wordpress.com/2012/05/25/gentoo-config-for-us-international-keyboard-layout/
 # see: https://zuttobenkyou.wordpress.com/2011/08/24/xorg-using-the-us-international-altgr-intl-variant-keyboard-layout/
 Section "InputClass"
-    Identifier "Default keyboard"
+    Identifier "Default Keyboard"
     MatchIsKeyboard "on"
     Option "XkbLayout" "us"
     Option "XkbVariant" "altgr-intl"
